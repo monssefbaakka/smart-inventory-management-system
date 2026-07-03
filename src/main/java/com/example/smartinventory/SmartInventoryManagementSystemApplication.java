2c9a67d3a29aaf55d15ca7d4aac80920a6887818
monssefbaakka <baakkamonssef@gmail.com>
2026-07-03 21:04:28 +0100

    feat: initialize Maven project structure with Spring Boot

---
+++ src/main/java/com/example/smartinventory/SmartInventoryManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.smartinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartInventoryManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartInventoryManagementSystemApplication.class, args);
+	}
+
+}
